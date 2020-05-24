30bc80266ef793b0d0e3de4b05aa208f6a07a8d0
Dev2 <Dev2@gmail.com>
2020-05-25 00:18:07 +0530

    Dev1 applied stached changes

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -13,19 +13,27 @@ public class Utility {
 		System.out.println("hello");
 		System.out.println("Jai Hind!");
 	}
-	
+
 	public void m2() {
 		System.out.println("Utility.m2()");
 	}
-	
+
 	public void m3() {
 		System.out.println("Utility.m2()");
 	}
-	
+
 	public void m4() {
 		System.out.println("Utility.m5()");
 	}
-	
+
+	public void m5() {
+		System.out.println("Utility.m5()");
+	}
+
+	public void m6() {
+		System.out.println("m6");
+	}
+
 	public void m7() {
 		System.out.println("Utility.m7()");
 	}
